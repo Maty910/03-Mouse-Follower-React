90dccf3c4d7dacdc37415d1ca623d86fbc779503
Maty910 <matychacong@gmail.com>
2024-09-16 14:46:12 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] =useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('effect ', { enabled })
+    
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x:clientX, y:clientY})
+    }
+
+    if (enabled) {
+    window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove' , handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div style={{
+          position: 'absolute',
+          backgroundColor: 'green',
+          border: '1px solid white',
+          borderRadius: '20%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -22,
+          top: 0,
+          width: 50,
+          height: 20,
+          transform: `translate(${position.x}px, ${position.y}px)`}}
+        />
+        <button onClick={() => setEnabled(!enabled)}>
+          {enabled ? 'Desactivar' : 'Activar'} seguir mouse
+        </button>
+    </>
+  )
+}
+
+function App () {
+  const [mounted, setMounted] = useState(true)
+
+  return (
+    <main>
+      {mounted && <FollowMouse />}
+      <button onClick={() => setMounted(!mounted)}>
+        Activar componente FollowMouse
+      </button>
+    </main>
+  )
+}
+
+export default App
